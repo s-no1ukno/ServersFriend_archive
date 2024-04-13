cdbd1a0cb215933c0c6f521ab343c171f4ebc82d
Jordan Snow <jordan@snowdev.co>
2024-04-13 15:23:34 -0600

    fix: Shift List Scrollable View bugfix

--- ServersFriend/Views/SettingsView.swift
+++ ServersFriend/Views/SettingsView.swift
@@ -27,7 +27,7 @@ struct SettingsView: View {
 
   }
   
-  func formatTipText(bool: Bool) -> Text {
+  private func formatTipText(bool: Bool) -> Text {
     if bool == false {
       return Text("No").foregroundColor(.red)
     } else {
@@ -42,44 +42,40 @@ struct SettingsView: View {
         Text("Add a new shift to start adding tips!")
       } else {
         List {
-          ScrollView {
-            Section {
-              ForEach(items) { item in
-                VStack {
-                  HStack {
-                    Text(item.nameOfShift)
-                      .bold()
-                    Spacer()
-                    Text(String(format: "$ %.2f per hour", item.hourlyWage))
-                      .bold()
-                  }
-                  .padding(.top, 10)
-                  .padding(.bottom, 10)
-                  HStack {
-                    VStack(alignment: .leading) {
-                      Text("Tracking Tip In? \(formatTipText(bool: item.tipIn))")
-                      Text("Tracking Tip Out? \(formatTipText(bool: item.tipOut))")
-                    }
-                    .font(.caption)
+          Section {
+            ForEach(items) { item in
+              VStack {
+                HStack {
+                  Text(item.nameOfShift)
                     .bold()
-                    
-                    Spacer()
-                  }
+                  Spacer()
+                  Text(String(format: "$ %.2f per hour", item.hourlyWage))
+                    .bold()
                 }
-                .swipeActions {
-                  Button("Delete") {
-                    print("delete item here")
+                .padding(.top, 10)
+                .padding(.bottom, 10)
+                HStack {
+                  VStack(alignment: .leading) {
+                    Text("Tracking Tip In? \(formatTipText(bool: item.tipIn))")
+                    Text("Tracking Tip Out? \(formatTipText(bool: item.tipOut))")
                   }
-                  .tint(.red)
+                  .font(.caption)
+                  .bold()
+                  
+                  Spacer()
                 }
-                Divider()
               }
-            } header: {
-              Text("Current Shift Types")
-                .foregroundColor(.blue)
-                .bold()
+              .swipeActions {
+                Button("Delete") {
+                  print("delete item here")
+                }
+                .tint(.red)
+              }
             }
-
+          } header: {
+            Text("Current Shift Types")
+              .foregroundColor(.blue)
+              .bold()
           }
         }
         .listStyle(InsetGroupedListStyle())
